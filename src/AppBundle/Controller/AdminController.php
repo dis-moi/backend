0631221285e9fdecb5b031502e6c3c79ed660c6f
lilobase <alemaire@quantis.fr>
2016-08-04 15:20:36 +0200

    remove filter & easy admin overrides

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -20,24 +20,5 @@ class AdminController extends BaseAdminController
     {
         $this->get('fos_user.user_manager')->updateUser($user, false);
     }
-
-    protected function listRecommendationAction(){
-        $this->dispatch(EasyAdminEvents::PRE_LIST);
-
-        $fields = $this->entity['list']['fields'];
-        $paginator = $this->findAll($this->entity['class'], $this->request->query->get('page', 1), $this->config['list']['max_results'], $this->request->query->get('sortField'), $this->request->query->get('sortDirection'));
-
-        $this->dispatch(EasyAdminEvents::POST_LIST, array('paginator' => $paginator));
-
-        $preview_data = $this->getDoctrine()
-            ->getRepository('AppBundle:MatchingContext')
-            ->findAllPrivate();
-
-        return $this->render($this->entity['templates']['list'], array(
-            'preview_data' => $preview_data,
-            'paginator' => $paginator,
-            'fields' => $fields,
-            'delete_form_template' => $this->createDeleteForm($this->entity['name'], '__id__')->createView(),
-        ));
-    }
+    
 }
