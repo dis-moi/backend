f4af601ddceb97fbfb688a86a9545482fbf4d95b
François Cadeillan <francois@azsystem.fr>
2016-08-08 13:45:00 +0200

    Add missing migration and code from PR#2; Force default value for visibility;

---
+++ app/DoctrineMigrations/Version20160808112313.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160808112313 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE recommendation DROP FOREIGN KEY FK_433224D2953C1C61');
+        $this->addSql('DROP INDEX UNIQ_433224D2953C1C61 ON recommendation');
+        $this->addSql('ALTER TABLE recommendation DROP source_id');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE recommendation ADD source_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE recommendation ADD CONSTRAINT FK_433224D2953C1C61 FOREIGN KEY (source_id) REFERENCES source (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_433224D2953C1C61 ON recommendation (source_id)');
+    }
+}
