2adfa0c6d2bd58efcdc7679771b0cb7b4b8f707c
Mikaël Létang <mikael.letang@gmail.com>
2016-10-06 16:19:55 +0200

    reactivate https; accept the load balancer as trusted proxy

--- web/app.php
+++ web/app.php
@@ -25,6 +25,7 @@ $kernel->loadClassCache();
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+Request::setTrustedProxies([$request->server->get('REMOTE_ADDR')]);
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
