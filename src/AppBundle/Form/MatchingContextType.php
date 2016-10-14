27fd3de98f9cc9a0395eb3cbfccc66cf28e7c997
Mikaël Létang <mikael.letang@gmail.com>
2016-10-14 18:02:06 +0200

    remove changes in form, they have no effect

--- src/AppBundle/Form/MatchingContextType.php
+++ src/AppBundle/Form/MatchingContextType.php
@@ -3,9 +3,6 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,8 +15,8 @@ class MatchingContextType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description', TextType::class, ['required' => false])
-            ->add('urlRegex', TextType::class, ['required' => true])
+            ->add('description')
+            ->add('urlRegex')
         ;
     }
     
