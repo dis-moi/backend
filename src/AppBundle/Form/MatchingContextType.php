652f6d8adf710c16b8f470e56c4c1d2142031795
Mikaël Létang <mikael.letang@gmail.com>
2016-10-14 14:50:59 +0200

    validation of embedded forms (WIP)

--- src/AppBundle/Form/MatchingContextType.php
+++ src/AppBundle/Form/MatchingContextType.php
@@ -4,6 +4,8 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,8 +18,8 @@ class MatchingContextType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
-            ->add('urlRegex')
+            ->add('description', TextType::class, ['required' => false])
+            ->add('urlRegex', TextType::class, ['required' => true])
         ;
     }
     
