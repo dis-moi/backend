6f74fff5f79501776c8f5e1f290a173e06f3b950
Mikaël Létang <mikael.letang@gmail.com>
2016-11-03 11:08:10 +0100

    additional checks

--- web/js/regex_validator.js
+++ web/js/regex_validator.js
@@ -2,7 +2,7 @@
   validate_url = function(url) {
     var empty_regex_message = 'L\'url ne peut être vide';
     var invalid_regex_message = 'Cette regexp est invalide';
-    var catch_all_regex_msg = 'Cette regexp est trop large; elle couvre à la fois http://google.com et http://lemonde.fr';
+    var catch_all_regex_msg = 'Cette regexp est trop large';
 
     //Regex empty
     if (!url) {
@@ -18,8 +18,19 @@
     }
 
     //Regex matching too many urls
+    var state = {success: true};
     if (regex.test('http://google.com') && regex.test('http://lemonde.fr')) {
-      return {success: false, message: catch_all_regex_msg};
+      state.success = false;
+      state.message = catch_all_regex_msg + " : elle couvre http://google.com et http://lemonde.fr";
+    } else if (regex.test('http://google.com?foo=bar&bar=foo')) {
+      state.success = false;
+      state.message = catch_all_regex_msg + " : elle couvre http://google.com?foo=bar&bar=foo";
+    } else if (regex.test('http://google.com/foo/bar')) {
+      state.success = false;
+      state.message = catch_all_regex_msg + " : elle couvre http://google.com/foo/bar";
+    }
+    if (!state.success) {
+      return state;
     }
 
     return {success: true, message: 'Regex valide'}
