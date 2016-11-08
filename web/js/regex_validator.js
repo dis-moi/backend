8c3b7d10b3f08038453f3c5530e9ce2fcc98e49b
Mikaël Létang <mikael.letang@gmail.com>
2016-11-08 20:24:56 +0100

    prevent saving the form if urls are invalid

--- web/js/regex_validator.js
+++ web/js/regex_validator.js
@@ -46,12 +46,13 @@
         if (!status.success) {
           show_error_message($(this), status.message);
         }
+        disable_form_submission_if_needed($(this));
       });
     }
   };
 
   var show_error_message = function(element, message) {
-    element.parent().addClass('has-error').append(
+    element.parent().addClass('has-error').addClass('has-js-error').append(
       '<div class="error-block"><span class="label label-danger">Erreur</span>'
       +message
       +'</div>'
@@ -60,7 +61,21 @@
 
   var remove_error_message = function(element) {
     element.parent().removeClass('has-error');
+    element.parent().removeClass('has-js-error');
     element.siblings('.error-block').remove();
+  };
+
+  var disable_form_submission_if_needed = function(element) {
+    var submit_button = element.parents('form').find('button.btn-primary');
+    if ($('.has-js-error').size()) {
+      submit_button.attr('disabled', 'disabled');
+    } else {
+      submit_button.removeAttr('disabled');
+    }
+  };
+
+  var disable_form_submission = function(element) {
+
   };
 
 })();
