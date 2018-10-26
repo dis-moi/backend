89918943d66bf0ffeab2a76a918680d2b1dc0b26
Charles-Elie <charles-elie@cethyworks.com>
2018-10-26 13:09:02 +0200

    Add MatchingContext::excludeUrlRegex + update /api/v2/matchingcontexts resource

--- src/AppBundle/Entity/BrowserExtension/MatchingContext.php
+++ src/AppBundle/Entity/BrowserExtension/MatchingContext.php
@@ -7,13 +7,15 @@ class MatchingContext
 {
     public $recommendation_url;
     public $url_regex;
+    public $exclude_url_regex;
 
     /**
      * MatchingContext constructor.
      */
-    public function __construct($recommendation_url, $url_regex)
+    public function __construct($recommendation_url, $url_regex, $exclude_url_regex)
     {
         $this->recommendation_url = $recommendation_url;
         $this->url_regex = $url_regex;
+        $this->exclude_url_regex = $exclude_url_regex;
     }
 }
