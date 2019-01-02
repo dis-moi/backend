cd4266ca94b952324affdcfeea0c0f3c8a1d2d68
Charles-Elie <charles-elie@cethyworks.com>
2019-01-02 11:12:35 +0100

    Fix DataConverter tests

--- tests/AppBundle/Helper/DataConverterTest.php
+++ tests/AppBundle/Helper/DataConverterTest.php
@@ -32,17 +32,16 @@ class DataConverterTest extends TestCase
         $expectedContent = "<p>My simple content</p>\n<p>With some other content. </p>";
         $this->assertEquals($expectedContent, DataConverter::convertNewLinesToParagraphs($content));
     }
-/*
+
     public function testItAddUtmSourceToUrl()
     {
-        $add_utm_source = NoticeFactory::add_utm_source("adomain.fr?query=param");
+        $add_utm_source = DataConverter::addUtmSourceToLink("adomain.fr?query=param");
         $this->assertEquals("adomain.fr?query=param&utm_source=lmem_assistant", $add_utm_source);
     }
 
     public function testItAddUtmSourceToUrlWithoutQueryParam()
     {
-        $add_utm_source = NoticeFactory::add_utm_source("http://www.adomain.fr");
+        $add_utm_source = DataConverter::addUtmSourceToLink("http://www.adomain.fr");
         $this->assertEquals("http://www.adomain.fr?utm_source=lmem_assistant", $add_utm_source);
     }
-    */
 }
