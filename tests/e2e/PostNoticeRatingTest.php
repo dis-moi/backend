ecc510262bb8747eb491c0e9a33a76cc9f632469
Benjamin Menant <bmenant@lmem.net>
2019-03-01 17:13:07 +0100

    fix(postRatingController): return json error 422 on wrong type

--- tests/e2e/PostNoticeRatingTest.php
+++ tests/e2e/PostNoticeRatingTest.php
@@ -24,4 +24,23 @@ class PostNoticeRatingTest extends BaseApiE2eTestCase
         static::$client->request('POST', '/api/v3/notices/'. $notice->getId() .'/ratings', [], [], [], $content);
         $this->assertEquals(204, static::$client->getResponse()->getStatusCode());
     }
+
+    public function testPostNoticeRatingWrongType()
+    {
+        /** @var Notice $notice */
+        $notice = static::$referenceRepository->getReference('notice_type_ecology');
+
+        $content = json_encode([
+            'ratingType' => 'wrongtype',
+            'context' => [
+                'datetime' => '2018-11-24T18:00:00Z',
+                'url' => 'https://en.wikipedia.org/',
+                'geolocation' => 'geo:37.786971,-122.399677;u=35'
+            ],
+            'reason' => 'foo'
+        ]);
+
+        static::$client->request('POST', '/api/v3/notices/'. $notice->getId() .'/ratings', [], [], [], $content);
+        $this->assertEquals(422, static::$client->getResponse()->getStatusCode());
+    }
 }
