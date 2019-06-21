45185867f5eba6bd4e2617efe5930935345bef9c
Benjamin Menant <bmenant@lmem.net>
2019-06-21 12:13:51 +0200

    chore(docker): install packages and migrations at startup

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -6,13 +6,16 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm "$@"
 fi
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
-	mkdir -p var/cache var/log
-	#setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	#setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+if [ ! -e 'vendor/autoload.php' ]; then
+    composer install --no-progress --no-suggest
+    bin/console assets:install web
 
-	#bin/console doctrine:migrations:migrate --no-interaction
-    #bin/console doctrine:fixtures:load -n
+    echo Fixing assets access rights...
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+    bin/console doctrine:migrations:migrate -n
+    bin/console doctrine:fixtures:load -n
 fi
 
 exec docker-php-entrypoint "$@"
