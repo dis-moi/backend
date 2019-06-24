2d70970d35b3b20c697686c73b656b9177b451e4
Benjamin Menant <bmenant@lmem.net>
2019-06-24 13:14:09 +0200

    feat(contributors): add avatar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
