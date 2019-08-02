e907552acc2b5e4044c1bbe5e9956bd2455ebfed
Benjamin Menant <bmenant@lmem.net>
2019-08-02 23:33:41 +0200

    fix(regex_validator): support domain name

--- web/js/regex_validator.js
+++ web/js/regex_validator.js
@@ -61,29 +61,53 @@
         return new RegExpStateSuccess();
     }
 
-    function validateFields(regexField, exampleField) {
+    function validateFields(regexField, dnField, exampleField) {
         const loose = regexField.id.startsWith('restricted');
+        const regexValue = !!dnField.value ?
+            dnField.value.replace(/\./g, '\\.') + regexField.value :
+            regexField.value;
+
         const status = validate_url_regexp(
-            regexField.value,
+            regexValue,
             exampleField.value,
             loose);
 
         if (typeof status === 'undefined' || status instanceof RegExpStateSuccess) {
             regexField.setCustomValidity('');
+            removeErrorClass(regexField, dnField, exampleField);
         }
         else if (status instanceof RegExpStateError) {
             regexField.setCustomValidity(status.message);
+            addErrorClass(regexField, dnField, exampleField);
         }
     }
 
-    jQuery(($) => {
+    function selectFields(child) {
         const parentSelector = '[id*="matchingContexts_"]';
+        return [
+            jQuery(child).parents(parentSelector).find('[id$="urlRegex"]')[0],
+            jQuery(child).parents(parentSelector).find('[id$="domainName"]')[0],
+            jQuery(child).parents(parentSelector).find('[id$="exampleUrl"]')[0],
+        ];
+    }
 
-        $('form [id$="urlRegex"]').change((event) => {
-            validateFields(event.target, $(event.target).parents(parentSelector).find('[id$="exampleUrl"]')[0]);
-        });
-        $('form [id$="exampleUrl"]').change((event) => {
-            validateFields($(event.target).parents(parentSelector).find('[id$="urlRegex"]')[0], event.target);
-        });
+    function addErrorClass(regexField, dnField, exampleField) {
+        const parentSelector = '.field-matching_context .form-group';
+        jQuery(regexField).parents(parentSelector).addClass('has-error');
+        jQuery(dnField).parents(parentSelector).addClass('has-error');
+        jQuery(exampleField).parents(parentSelector).addClass('has-warning');
+    }
+
+    function removeErrorClass(regexField, dnField, exampleField) {
+        const parentSelector = '.field-matching_context .form-group';
+        jQuery(regexField).parents(parentSelector).removeClass('has-error');
+        jQuery(dnField).parents(parentSelector).removeClass('has-error');
+        jQuery(exampleField).parents(parentSelector).removeClass('has-warning');
+    }
+
+    jQuery(($) => {
+        $('form [id$="urlRegex"]').change((event) => validateFields(...selectFields(event.target)));
+        $('form [id$="exampleUrl"]').change((event) => validateFields(...selectFields(event.target)));
+        $('form [id$="domainName"]').change((event) => validateFields(...selectFields(event.target)));
     });
 })();
