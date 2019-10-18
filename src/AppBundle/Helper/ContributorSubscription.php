3f4b12c5d7cce8d12c2504ef1e8dd0686e370ed9
Benjamin Menant <bmenant@lmem.net>
2019-10-18 14:51:25 +0200

    fix(contributor rating): drop useless stuff

--- src/AppBundle/Helper/ContributorSubscription.php
+++ src/AppBundle/Helper/ContributorSubscription.php
@@ -8,13 +8,6 @@ class ContributorSubscription extends Enum {
     const SUBSCRIBE = 'subscribe';
     const UNSUBSCRIBE = 'unsubscribe';
 
-//    static function getDefault(){
-//        /**
-//         * How this is working ? o.o
-//         */
-//        return self::SUBSCRIBE();
-//    }
-
     public function __toString(){
         return self::getValue();
     }
