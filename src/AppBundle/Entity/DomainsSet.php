54fff6a882d4a9642f3cc66e723af80dbce7b2e1
Jalil Arfaoui <jalil@arfaoui.net>
2020-02-19 18:40:29 +0100

    refactor: Use DoctrineBehaviors instead of TimestampableEntity

--- src/AppBundle/Entity/DomainsSet.php
+++ src/AppBundle/Entity/DomainsSet.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
 
 /**
  * Domain
@@ -12,8 +13,10 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="domains_set")
  * @ORM\Entity
  */
-class DomainsSet extends TimestampableEntity
+class DomainsSet
 {
+  use ORMBehaviors\Timestampable\Timestampable;
+
   /**
    * @var int
    *
