b00208288e2f69ced5020289eaf434e4c1242eeb
Jalil Arfaoui <jalil@arfaoui.net>
2020-05-06 18:12:02 +0200

    feat(notices): initialize notice expiration to one year later

--- src/AppBundle/EntityListener/CreateNoticeTrait.php
+++ src/AppBundle/EntityListener/CreateNoticeTrait.php
@@ -3,7 +3,6 @@
 namespace AppBundle\EntityListener;
 
 use AppBundle\Entity\Notice;
-use AppBundle\Service\DateTimeImmutable;
 use DateTime as DateTime;
 
 trait CreateNoticeTrait
@@ -11,7 +10,5 @@ trait CreateNoticeTrait
     protected function createNotice(Notice $notice)
     {
         $notice->setCreated(new DateTime());
-
-        $notice->setInitialExpires(new DateTimeImmutable());
     }
 }
