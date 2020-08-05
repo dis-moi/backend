6068c0b8d595282adf0d6a821104140719ee43cd
Jalil Arfaoui <jalil@arfaoui.net>
2020-08-06 00:44:22 +0200

    feat: add pagination to /api/v3/notices

--- tests/e2e/GetNoticesTest.php
+++ tests/e2e/GetNoticesTest.php
@@ -2,29 +2,46 @@
 
 namespace Tests\e2e;
 
-use AppBundle\Entity\Notice;
-
 class GetNoticesTest extends BaseApiE2eTestCase
 {
     public function testGetNotice(): void
     {
-        /** @var Notice $validNotice */
-        $validNotice = static::$referenceRepository->getReference('notice_type_ecology');
-        $privateNotice = static::$referenceRepository->getReference('notice_private');
-        $archivedNotice = static::$referenceRepository->getReference('notice_type_ecology_archived');
-        $expiredNotice = static::$referenceRepository->getReference('notice_expired_unpublished');
-        $noticeByDisabledContributor = static::$referenceRepository->getReference('notice_disabled');
-
         $payload = $this->makeApiRequest('/api/v3/notices');
+
+        self::assertCount(8, $payload);
 
         $ids = array_map(static function ($noticeData) {
             return $noticeData['id'];
         }, $payload);
 
-        $this->assertContains($validNotice->getId(), $ids);
-        $this->assertNotContains($privateNotice->getId(), $ids);
-        $this->assertNotContains($archivedNotice->getId(), $ids);
-        $this->assertNotContains($expiredNotice->getId(), $ids);
-        $this->assertNotContains($noticeByDisabledContributor->getId(), $ids);
+        self::assertContains(static::$referenceRepository->getReference('notice_type_ecology')->getId(), $ids);
+        self::assertNotContains(static::$referenceRepository->getReference('notice_private')->getId(), $ids);
+        self::assertNotContains(static::$referenceRepository->getReference('notice_type_ecology_archived')->getId(), $ids);
+        self::assertNotContains(static::$referenceRepository->getReference('notice_expired_unpublished')->getId(), $ids);
+        self::assertNotContains(static::$referenceRepository->getReference('notice_disabled')->getId(), $ids);
+    }
+
+    public function testGetPaginatedNotices(): void
+    {
+        $firstPage = array_map(
+            static function ($noticeData) {
+                return $noticeData['id'];
+            },
+            $this->makeApiRequest('/api/v3/notices?limit=4')
+        );
+
+        $nextPage = array_map(
+            static function ($noticeData) {
+                return $noticeData['id'];
+            },
+            $this->makeApiRequest('/api/v3/notices?limit=4&offset=4')
+        );
+
+        self::assertCount(4, $firstPage);
+        self::assertCount(4, $nextPage);
+
+        $diff = array_diff($firstPage, $nextPage);
+
+        self::assertCount(4, $diff);
     }
 }
