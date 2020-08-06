04fa779d9b4e720b5e8f7fe258a6c9befdb04752
Jalil Arfaoui <jalil@arfaoui.net>
2020-08-06 15:13:27 +0200

    feat: remove default path in domain names

--- tests/e2e/GetMatchingContextsTest.php
+++ tests/e2e/GetMatchingContextsTest.php
@@ -4,10 +4,10 @@ namespace Tests\e2e;
 
 class GetMatchingContextsTest extends BaseApiE2eTestCase
 {
-    public function getMatchingContextsData()
+    public function getMatchingContextsData(): array
     {
         return [
-            [null, 6, ['http://site-ecologique.fr', 'http://random-site.fr', 'http://site-ecologique-et-politique.fr', 'http://expired.fr', "(duckduckgo\.com/|www\.bing\.com/|www\.google\.fr/|www\.qwant\.com/|www\.yahoo\.com/|first\.domainname\.fr/|second\.domainname\.fr/)/superexample", 'http://siteecologique.fr']],
+            [null, 6, ['http://site-ecologique.fr', 'http://random-site.fr', 'http://site-ecologique-et-politique.fr', 'http://expired.fr', "(duckduckgo\.com|www\.bing\.com|www\.google\.fr|www\.qwant\.com|www\.yahoo\.com|first\.domainname\.fr|second\.domainname\.fr)/superexample", 'http://siteecologique.fr']],
             [['john_doe', 'contributor2'], 5, ['http://site-ecologique.fr', 'http://site-ecologique-et-politique.fr', 'http://random-site.fr', 'http://expired.fr', 'http://siteecologique.fr']],
             [['john_doe'], 3, ['http://site-ecologique.fr', 'http://random-site.fr', 'http://siteecologique.fr']],
             [['contributor2'], 2, ['http://site-ecologique-et-politique.fr', 'http://expired.fr']],
@@ -17,23 +17,23 @@ class GetMatchingContextsTest extends BaseApiE2eTestCase
     /**
      * @dataProvider getMatchingContextsData
      */
-    public function testGetMatchingContexts(?array $contributors, int $count, array $urlRegexes)
+    public function testGetMatchingContexts(?array $contributors, int $count, array $urlRegexes): void
     {
         $url = '/api/v3/matchingcontexts';
         if ($contributors) {
-            $url .= '?'.implode('&', array_map(function ($contributorReference) {
+            $url .= '?'.implode('&', array_map(static function ($contributorReference) {
                 return 'contributors[]='.static::$referenceRepository->getReference($contributorReference)->getId();
             }, $contributors));
         }
         $payload = $this->makeApiRequest($url);
 
-        $this->assertEquals($count, count($payload));
+        self::assertCount($count, $payload);
 
         foreach ($payload as $matchingContext) {
-            $this->assertEquals(1, preg_match('/^http.*\/api\/v3\/notices\/.*$/', $matchingContext['noticeUrl']));
+            self::assertRegExp('/^http.*\/api\/v3\/notices\/.*$/', $matchingContext['noticeUrl']);
         }
 
-        $this->assertEqualsCanonicalizing($urlRegexes, array_map(function ($matchingContext) {
+        self::assertEqualsCanonicalizing($urlRegexes, array_map(static function ($matchingContext) {
             return $matchingContext['urlRegex'];
         }, $payload));
     }
