dd5b4f6654792282428639a8ee0fe8d3bc684631
Jalil Arfaoui <jalil@arfaoui.net>
2020-11-01 16:49:06 +0100

    test(profiles): up to 5 pinned notices by Contributor

--- tests/Entity/ContributorTest.php
+++ tests/Entity/ContributorTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Entity;
 use App\Entity\Contributor;
 use App\Entity\Notice;
 use App\Tests\FixtureAwareWebTestCase;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class ContributorTest extends FixtureAwareWebTestCase
 {
@@ -28,5 +29,20 @@ class ContributorTest extends FixtureAwareWebTestCase
 
         $mostLikedOrDisplayedNotice = $contributor->getTheirMostLikedOrDisplayedNotice();
         $this->assertEquals($notice->getId(), $mostLikedOrDisplayedNotice->getId());
+    }
+
+    public function testItAddsPinnedNotice()
+    {
+        /** @var Contributor $contributor */
+        $contributor = $this->referenceRepository->getReference('famous_contributor');
+
+        $noticeEcology = $this->referenceRepository->getReference('notice_type_ecology_and_politics');
+        $noticeDisplayed = $this->referenceRepository->getReference('notice_displayed');
+        $noticeLiked = $this->referenceRepository->getReference('notice_liked');
+        $noticeLikedDisplayed = $this->referenceRepository->getReference('notice_liked_displayed');
+
+        $contributor->setPinnedNotices(new ArrayCollection([$noticeLikedDisplayed, $noticeEcology, $noticeLiked, $noticeDisplayed]));
+
+        self::assertCount(4, $contributor->getPinnedNotices());
     }
 }
