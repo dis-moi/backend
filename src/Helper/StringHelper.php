5b09dbe42ee1509e8882f97b1033f8e25fe75b27
Jalil Arfaoui <jalil@arfaoui.net>
2020-11-03 16:27:40 +0100

    feat(admin): shorter notices in pinned

---
+++ src/Helper/StringHelper.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Helper;
+
+/**
+ * Class StringHelper
+ */
+class StringHelper {
+    private function __construct () {
+
+    }
+
+    /**
+     * @param string $string
+     * @param int $length
+     * @return string
+     */
+    public static function truncate(string $string, int $length): string
+    {
+        if (\strlen($string) <= $length) {
+            return $string;
+        }
+
+        $text = substr($string, 0, $length);
+
+        return substr($text, 0, strrpos($text, ' ')).' …';
+    }
+}
