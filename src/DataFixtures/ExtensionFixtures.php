a4479160e3ef5f1f8072727e0adbd71f651f0a27
lutangar <johan.dufour@gmail.com>
2021-03-03 13:32:13 +0100

    fix: Loading Extension fixtures

--- src/DataFixtures/ExtensionFixtures.php
+++ src/DataFixtures/ExtensionFixtures.php
@@ -11,14 +11,17 @@ class ExtensionFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $extension1 = new Extension('EXT1');
+        $extension1->confirm();
         $this->addReference('extension_1', $extension1);
         $manager->persist($extension1);
 
         $extension2 = new Extension('EXT2');
+        $extension2->confirm();
         $this->addReference('extension_2', $extension2);
         $manager->persist($extension2);
 
         $extension3 = new Extension('EXT3');
+        $extension3->confirm();
         $this->addReference('extension_3', $extension3);
         $manager->persist($extension3);
 
