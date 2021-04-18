a96701004004fe98a1a28159d15c959053968bb2
Goutte <antoine@goutenoir.com>
2021-04-19 00:29:16 +0200

    feat: backbone of v4 normalizer

---
+++ src/Serializer/V4/Ability/Versioning.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Serializer\V4\Ability;
+
+
+use App\Serializer\V3\NormalizerOptions;
+
+
+trait Versioning
+{
+    public function getVersionFromContext(array $context)
+    {
+        // we could also grab it from url prefix as a fallback
+        return $context[NormalizerOptions::VERSION] ?? null;
+    }
+
+    public function isForV4(array $context)
+    {
+        return 4 === $this->getVersionFromContext($context);
+    }
+}
