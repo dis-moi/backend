6624c2a375f378352977bf51711587a58e563bc8
Goutte <antoine@goutenoir.com>
2021-04-20 05:05:44 +0200

    feat: add hats to users
    See https://github.com/dis-moi/backend/issues/12#issuecomment-242132282

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use App\Entity\Contributor;
 
 /**
+ * Everyone's a User.
+ * Holds the auth nitty-gritty (see parent FOS).
+ * A User may control multiple Contributors, ie. wear many hats.
+ *
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
@@ -20,8 +26,41 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * Contributors this User may impersonate, that is can submit Notices as.
+     *
+     * @var ArrayCollection<Contributor>
+     * @ORM\ManyToMany(
+     *     targetEntity=Contributor::class,
+     *     mappedBy="impersonators",
+     *     cascade={"persist"},
+     * )
+     */
+    private $hats;
+
     public function __construct()
     {
         parent::__construct();
+        $this->hats = new ArrayCollection();
+    }
+
+    /**
+     * @return ArrayCollection<Contributor>
+     */
+    public function getHats()
+    {
+        return $this->hats;
+    }
+
+    public function addHat(Contributor $contributor)
+    {
+        $this->hats->add($contributor);
+    }
+
+    public function removeHat(Contributor $contributor)
+    {
+        if ($this->hats->contains($contributor)) {
+            $this->hats->removeElement($contributor);
+        }
     }
 }
