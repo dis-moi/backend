dis-moi/backend

commit fed695c806bf23e20473e496c8049cecae5f5c3e

chore: Setup PhpCsFixer together with PhpStan
Add more CS rules
Setup PhpStan with a few exceptions and set it as pre-commit hooks.
Refactor repositories to use new repository service class.
Fix all errors, tests and code style issues.

src/Controller/Api/GetNoticesAction.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Api;
 
 use App\Repository\NoticeRepository;
 use App\Serializer\NormalizerOptions;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -27,16 +30,16 @@ class GetNoticesAction extends BaseAction
      * @Route("/notices")
      * @Method("GET")
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
         $contributorId = $request->get('contributor', null);
         $limit = $request->get('limit', 1000);
         $offset = $request->get('offset', 0);
 
         if ($contributorId) {
-            $notices = $this->repository->getPageByContributor($contributorId, $limit, $offset);
+            $notices = $this->repository->getPageByContributor($contributorId, (int) $limit, (int) $offset);
         } else {
-            $notices = $this->repository->getPage($limit, $offset);
+            $notices = $this->repository->getPage((int) $limit, (int) $offset);
         }
 
         if (!is_iterable($notices)) {

commit b6fe8e9bc81ec32b85c77c5567f58a715dcf9995

fix: cannot fetch notices by contributor
cast contributorId in `GetNoticesAction` (broken by fed695c806bf23e20473e496c8049cecae5f5c3e)

(cherry picked from commit 961ba4bb7f18299f1b04726db31453db4840813a)

src/Controller/Api/GetNoticesAction.php
@@ -32,14 +32,14 @@ class GetNoticesAction extends BaseAction
      */
     public function __invoke(Request $request): JsonResponse
     {
-        $contributorId = $request->get('contributor', null);
-        $limit = $request->get('limit', 1000);
-        $offset = $request->get('offset', 0);
+        $contributorId = (int) $request->get('contributor', null);
+        $limit = (int) $request->get('limit', 1000);
+        $offset = (int) $request->get('offset', 0);
 
         if ($contributorId) {
-            $notices = $this->repository->getPageByContributor($contributorId, (int) $limit, (int) $offset);
+            $notices = $this->repository->getPageByContributor($contributorId, $limit, $offset);
         } else {
-            $notices = $this->repository->getPage((int) $limit, (int) $offset);
+            $notices = $this->repository->getPage($limit, $offset);
         }
 
         if (!is_iterable($notices)) {