53dd673ef686d01befd9ba531ee41a3c038ccfba
Goutte <antoine@goutenoir.com>
2021-04-26 07:42:46 +0200

    chore: clean up

--- src/Serializer/V4/NoticeNormalizer.php
+++ src/Serializer/V4/NoticeNormalizer.php
@@ -18,7 +18,6 @@ use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 class NoticeNormalizer
     implements
         ContextAwareNormalizerInterface,
-//        ContextAwareDenormalizerInterface,
         NormalizerAwareInterface
 {
 
@@ -76,37 +75,4 @@ class NoticeNormalizer
         return array_merge($base, $extra);
     }
 
-
-//    /**
-//     * {@inheritdoc}
-//     *
-//     * @param array $context options that denormalizers have access to
-//     */
-//    public function supportsDenormalization($data, $type, $format = null, array $context = [])
-//    {
-//        // TODO: Implement supportsDenormalization() method.
-//    }
-//
-//    /**
-//     * Denormalizes data back into an object of the given class.
-//     *
-//     * @param mixed $data Data to restore
-//     * @param string $type The expected class to instantiate
-//     * @param string $format Format the given data was extracted from
-//     * @param array $context Options available to the denormalizer
-//     *
-//     * @return object|array
-//     *
-//     * @throws BadMethodCallException   Occurs when the normalizer is not called in an expected context
-//     * @throws InvalidArgumentException Occurs when the arguments are not coherent or not supported
-//     * @throws UnexpectedValueException Occurs when the item cannot be hydrated with the given data
-//     * @throws ExtraAttributesException Occurs when the item doesn't have attribute to receive given data
-//     * @throws LogicException           Occurs when the normalizer is not supposed to denormalize
-//     * @throws RuntimeException         Occurs if the class cannot be instantiated
-//     * @throws ExceptionInterface       Occurs for all the other cases of errors
-//     */
-//    public function denormalize($data, $type, $format = null, array $context = [])
-//    {
-//        // TODO: Implement denormalize() method.
-//    }
 }
