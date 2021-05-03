6ea82b609833b40861ef3cc2c192bbb4974d7936
Goutte <antoine@goutenoir.com>
2021-05-03 13:29:18 +0200

    feat: add a (non-admin) test user with a hat

--- src/DataFixtures/UserAdminFixtures.php
+++ src/DataFixtures/UserAdminFixtures.php
@@ -4,43 +4,50 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
-use App\DataFixtures\Ability\Container;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
-
-class UserFixtures extends Fixture implements FixtureInterface, ContainerAwareInterface
+class UserAdminFixtures extends Fixture implements FixtureInterface, ContainerAwareInterface
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
-    use Container;
+    public function setContainer(ContainerInterface $container = null): void
+    {
+        $this->container = $container;
+    }
 
     public function load(ObjectManager $manager): void
     {
         $userManager = $this->container->get('fos_user.user_manager');
+
         $encoder = new MessageDigestPasswordEncoder('sha512', true, 5000);
 
         /** @var User $user */
         $user = $userManager->createUser();
 
-        $user->setUsername('test');
-        $user->setEmail('test@test.fr');
+        $user->setUsername('lmem');
+        $user->setEmail('infra@lmem.net');
         $user->setEnabled(true);
-        $user->setRoles(['ROLE_USER']);
-        $user->setPassword($encoder->encodePassword('test', $user->getSalt()));
-        $user->addHat($this->getReference('john_doe'));
+        $user->setRoles(['ROLE_SUPER_ADMIN']);
+
+        $password = $encoder->encodePassword('LM3M!P4SSW0RD', $user->getSalt());
+
+        $user->setPassword($password);
+
         $manager->persist($user);
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getOrder(): int
     {
-        return [
-            ContributorFixtures::class,
-        ];
+        return 2;
     }
-
 }
