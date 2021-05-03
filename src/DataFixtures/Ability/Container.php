e0e23bcd1185799156a0c421f4d989aa83352b0d
Goutte <antoine@goutenoir.com>
2021-05-03 13:29:18 +0200

    feat: add a (non-admin) test user with a hat

---
+++ src/DataFixtures/Ability/Container.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures\Ability;
+
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+trait Container
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null): void
+    {
+        $this->container = $container;
+    }
+}
