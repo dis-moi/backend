8001573694ab3a80a249d133eee06b93bcdf4d67
lutangar <johan.dufour@gmail.com>
2021-06-22 14:10:49 +0200

    chore: configure autowire for locale form type

--- src/Form/LocaleType.php
+++ src/Form/LocaleType.php
@@ -11,14 +11,33 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class LocaleType extends AbstractType
 {
+    /**
+     * @var string[]
+     */
+    private $supportedLocales = [];
+
+    /**
+     * LocaleType constructor.
+     *
+     * @param string[] $supportedLocales
+     */
+    public function __construct(array $supportedLocales)
+    {
+        $this->supportedLocales = $supportedLocales;
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'choices' => [
-                Locales::getName('fr') => 'fr',
-                Locales::getName('en') => 'en',
-                Locales::getName('br') => 'br',
-            ],
+            'choices' => array_combine(
+                array_map(
+                    function ($locale) {
+                        return Locales::getName($locale);
+                    },
+                    $this->supportedLocales
+                ),
+                $this->supportedLocales
+            ),
         ]);
     }
 
