5d34f8bece7c3e9f6c66c156ce62c21d6221fb52
lutangar <johan.dufour@gmail.com>
2021-07-20 10:34:15 +0200

    fix(oas): remove invalid ref property

--- src/OpenApi/Documenter/JwtDocumenter.php
+++ src/OpenApi/Documenter/JwtDocumenter.php
@@ -52,7 +52,7 @@ class JwtDocumenter implements DocumenterInterface
         ]);
 
         $jwt = new PathItem(
-            'jwt',
+            null,
             '',
             '',
             null,
